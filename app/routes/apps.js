e98463a566b9bc3b77d2ab6d85ed0387b2e1a499
Steve Luzynski <steve@luzynski.net>
2017-05-26 11:32:47 -0500

    updates for new API on back end

--- app/routes/apps.js
+++ app/routes/apps.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 
+// This has an issue to work through. I need to figure out the Ember
+// way to pass in the policy in play before this API call runs or it's
+// not going to work.
+
 export default Ember.Route.extend({
   model() {
-    return ['facebook', 'twitter', 'accuweather'];
+    let url = "http://127.0.0.1:5001/api/applications/";
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      let xhr = new XMLHttpRequest();
+
+      xhr.open('GET', url);
+      xhr.onreadystatechange = handler;
+      xhr.responseType = 'json';
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.send();
+
+      function handler() {
+        if (this.readyState == this.DONE) {
+          if (this.status == 200) {
+            resolve(this.response);
+          } else {
+            reject(new Error('getJSON: ' + url + 'failed with status: [' + this.status + ']'));
+          }
+        }
+      }
+    });
   }
 });
